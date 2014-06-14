f2df88d45cef7a98c5552a7da908a000c9614c34
Dave Nelson <davidicus21@gmail.com>
2014-06-14 15:27:41 -0600

    Flash notices

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def show_flash_message
+    unless flash.empty?
+      [:alert, :failed].each do |key|
+        return content_tag(:div, flash[key], class: "flash-#{key}") if flash[key].present?
+      end
+    end
+  end
 end
