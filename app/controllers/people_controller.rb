2411b6382c0a9486b71461d3598af5248955a769
Dave Nelson <davidicus21@gmail.com>
2014-06-14 10:47:50 -0600

    Finished people controller

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -9,18 +9,41 @@ class PeopleController < ApplicationController
   end
 
   def new
+    @person = Person.new
+  end
+
+  def create
+    @person = Person.new(person_params)
+    if @person.save
+      redirect_to people_path
+    else
+      render :new
+    end
+
   end
 
   def edit
   end
 
   def update
+    if @person.update_attributes(person_params)
+      redirect_to people_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @person.destroy
+    redirect_to people_path
   end
 
   private
+
+  def person_params
+    params.require(:person).permit(:name)
+    
+  end
 
   def find_person
     @person = Person.find(params[:id]) 
