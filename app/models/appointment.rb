9c00bb76ebfa5d7c4e64d3e9d509c24d552da830
Dave Nelson <davidicus21@gmail.com>
2014-06-14 16:05:48 -0600

    Fixed change cost bug

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,6 +1,6 @@
 class Appointment < ActiveRecord::Base
   belongs_to :person
   default_scope {order('time ASC')}
-
+  # validates :time, :dentist, :description, :estimated_cost, presence: true
 
 end
